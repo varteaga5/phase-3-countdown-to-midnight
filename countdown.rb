5b695cc406bb8f9575612e61801cb679f44857ff
vincent <vincent.arteaga@gmail.com>
2021-07-20 19:51:29 -0700

    commit tree cb6faca0c14a3a00a3159aaeefd467a924e23dd8

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+#countdown
+def countdown (int)
+    # outputs '<n> SECOND(S)!' string to STDOUT for each count (FAILED - 1)
+    while int >= 1 do
+       puts "#{int} SECOND(S)!" 
+       int -= 1
+    end
+    
+    "HAPPY NEW YEAR!"
+end
+
+#countdown_with_sleep
+def countdown_with_sleep (int)
+    # should take at least 5 seconds to execute (FAILED - 3)
+    while int >= 1 do
+        puts "#{int} SECOND(S)!" 
+        sleep(1)
+        int -= 1
+     end
+     puts "HAPPY NEW YEAR!"
 end
